df35d60098efe84aa4f7d89867403b275835d0b2
GarciaGarciaAramJesua <agarciag1912@alumno.ipn.mx>
2021-03-24 17:24:32 -0600

    formulariojs

---
+++ js/validarformulario.js
@@ -0,0 +1,67 @@
+function validar(formulario){
+
+    if(formulario.nombre.value.length < 5){
+        alert("Escribe más de 5 caracteres en el campo nombre");
+        formulario.nombre.focus();
+    return false;
+    }
+
+    var checkOK = "QWERTYUIOPASDFGHJKLÑZXCVBNM"
+    + "qweryuiopasdfghjklñzxcvbnm";
+
+    var checkStr = formulario.nombre.value;
+
+    var allvalid = true;
+
+    for(var i = 0; 1 < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOK.length; j++)
+        if(ch == checkOK.charAt(j))
+            break;
+        if(j == checkOK.length){
+            allvalid = false;
+            break;
+        }
+    }        
+
+    if(!allvalid){
+        alert("Escribe solo letras en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    var checkOK = "1234567890";
+
+    var checkStr = formulario.edad.value;
+
+    var allvalid = true;
+
+    for(var i = 0; 1 < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for(var j = 0; j < checkOK.length; j++)
+        if(ch == checkOK.charAt(j))
+            break;
+        if(j == checkOK.length){
+            allvalid = false;
+            break;
+        }
+    }        
+
+    if(!allvalid){
+        alert("Escribe solo numeros en el campo nombre");
+        formulario.edad.focus();
+        return false;
+    }
+
+    var txt = formulario.email.value;
+
+    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    alert("Email " + (b.test(txt)?"":" no ")+" valido");
+
+    return b.test(txt);
+
+
+}
+
+
